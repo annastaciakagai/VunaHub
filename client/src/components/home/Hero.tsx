803d68d930bf30e854c0df7d188c89e4c9ec2d37
annastaciakagai <kagaiannastacia@gmail.com>
2025-08-04 16:48:02 +0300

    Update hero.tsx

--- client/src/components/home/Hero.tsx
+++ client/src/components/home/Hero.tsx
@@ -64,11 +64,7 @@ const Hero = () => {
             className="hidden lg:block"
           >
             <img 
-<<<<<<< HEAD
               src="/VunaHub-logo.png" 
-=======
-              src="../../../public/VunaHub logo.png" 
->>>>>>> ffe9d185b3cdce4cd2ada61ac558050e9a8b02b1
               alt="VunaHub - Agricultural Collection and Distribution" 
               className="w-full h-auto rounded-2xl shadow-xl"
             />
